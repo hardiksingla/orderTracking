1f3cd375692b4aa23228a08a32403fb37f7bdf2c
hardiksingla <116436385+hardiksingla@users.noreply.github.com>
2024-06-20 12:07:22 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -67,6 +67,24 @@ interface OrderRequest {
     fullfilment_status: string;
 }
 
+const __dirname1 = path.resolve();
+console.log("17", path.resolve(__dirname, "../../frontend", "dist", "index.html"))
+
+if (true) {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  
+  app.get("/api/healthcheck", function(req,res){
+    res.json({ status: 'ok' })
+  
+  })
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../../frontend", "dist", "index.html"))
+  );  
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 app.post("/getOrders", async (req, res) => {
     console.log("hello")
